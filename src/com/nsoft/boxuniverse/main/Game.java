f80e490026991af2ce652108ea62829fe80830b4
DavidNexus <DavidNexus>
2017-03-16 19:22:45 +0100

    AÃadido sistema de renderizaciÃn de entidades no fÃsicas

--- src/com/nsoft/boxuniverse/main/Game.java
+++ src/com/nsoft/boxuniverse/main/Game.java
@@ -2,18 +2,27 @@ package com.nsoft.boxuniverse.main;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.nsoft.boxuniverse.world.BaseActor;
+import com.nsoft.boxuniverse.world.GameStage;
 
 public class Game extends ApplicationAdapter {
 
+	GameStage stage;
 	@Override
 	public void create() {
 		
+		stage = new GameStage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+		stage.debug();
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 	}
 	
 	@Override
 	public void render() {
 		
-		System.out.println(Gdx.graphics.getFramesPerSecond());
+		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+		stage.draw();
 	}
 	
 	@Override
